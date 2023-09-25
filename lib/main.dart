82bb3f8295fcdbb838a12526ae78de0781f1aa00
Christian <christian.535220055@stu.untar.ac.id>
2023-09-25 12:30:18 +0700

    verficationscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,15 @@
+import 'package:chatapp/Verification.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Chat App"),
-            
-            backgroundColor: Colors.blueAccent,
-          ),
-          body: Center(
-            child: 
-              TextField(
-                decoration: 
-                  InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Coba ketik',
-                  ),
-              ),
-           ),
-        ),
+      home: Verification()
     );
   }
 }
